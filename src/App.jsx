7e8162a0f5e1fb17b91b53f3605888f13e23286f
saad19987 <saadmushtaq@ragioneer.com>
2026-07-19 23:38:35 +0500

    implemented the menu section

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
-    <main>
+    <main className="overflow-clip">
       <Navbar />
       <Hero />
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 };
